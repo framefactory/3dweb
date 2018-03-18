40ac31a18ffa720e7a4a6e0643922e4ebbea9205
Andrin Rehmann <andrinrehmann@gmail.com>
2018-03-18 09:59:20 +0100

    Updated GUI, increased particles

--- source/scenes/Test.ts
+++ source/scenes/Test.ts
@@ -6,15 +6,20 @@ import dat from "dat.gui/build/dat.gui.module";
 export default class Test extends Scene
 {
     boxMeshes : THREE.Mesh[];
+    gui: dat.GUI;
+
+    speed: number;
+    adjTime: number;
+    rotationRadius: number;
 
     start(scene: THREE.Scene): THREE.Camera
     {
-        scene.background = new THREE.Color("darkblue");
-
+        scene.background = new THREE.Color("black");
+        this.adjTime = 0;
         const camera = new THREE.PerspectiveCamera(55, 1, 0.01, 100);
         camera.position.set(0, 0, 20);
 
-        const boxGeo = new THREE.BoxBufferGeometry(1, 1, 1);
+        const boxGeo = new THREE.SphereBufferGeometry(0.3,10,10);
         const boxMat = new THREE.MeshStandardMaterial({
             color: "#ffffff",
             metalness: 0,
@@ -22,9 +27,9 @@ export default class Test extends Scene
         });
 
         this.boxMeshes = [];
-        for (let i = 0; i<20; ++i){
+        for (let i = 0; i<100; ++i){
             const current = new THREE.Mesh(boxGeo,boxMat);
-            current.position.set(i-10,0,0);
+            current.position.set(0.3*(i-50),0,0);
             this.boxMeshes.push(current);
             scene.add(current);
 
@@ -32,14 +37,20 @@ export default class Test extends Scene
         //const boxMesh = new THREE.Mesh(boxGeo, boxMat);
         //scene.add(boxMesh);
 
-        const light = new THREE.PointLight("white",1,0,1);
+        const light = new THREE.PointLight("#00ffcc",1,0,1);
         light.position.set(-10,10,10);
         scene.add(light);
 
-        const light2 = new THREE.PointLight("red",1,0,1);
+        const light2 = new THREE.PointLight("#ff0066",1,0,1);
         light2.position.set(10,-10,-10);
         scene.add(light2);
-        
+
+        this.gui = new dat.GUI();
+        this.speed = 1;
+        this.gui.add(this, "speed", 0, 5, 0.05);
+
+        this.rotationRadius = 1;
+        this.gui.add(this, "rotationRadius", 0, 5, 0.05);
         
         console.log("hallo stadt");
         return camera;
@@ -47,10 +58,14 @@ export default class Test extends Scene
 
     update(time: number, delta: number)
     {
-        for (let i = 0; i<20; ++i){
-            this.boxMeshes[i].position.y = Math.sin(time+i);
-            this.boxMeshes[i].position.z = Math.cos(time+i);
+        this.adjTime += this.speed*delta;
+
+        for (let i = 0; i<100; ++i){
+
+            this.boxMeshes[i].position.y = this.rotationRadius*Math.sin(this.adjTime+i);
+            this.boxMeshes[i].position.z = this.rotationRadius*Math.cos(this.adjTime+i);
         }
+
     }
 
 }
